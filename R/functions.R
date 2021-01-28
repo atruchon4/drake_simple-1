930f96cbc81d0bf5d35398b4abb239b91a12097a
Alex <atruchon@vols.utk.edu>
2021-01-28 11:07:02 -0500

    Edited to plot different versions of the tree that was imported

--- R/functions.R
+++ R/functions.R
@@ -12,7 +12,9 @@ is_even <- function(phy) {
 }
 
 plot_tree <- function(phy, file) {
-	pdf(file=file)
+	pdf(file=file, width = 20, height = 20)
 	plot(phy)
+	plot(phy, type="fan")
+	plot(phy, type = "fan", show.tip.label=FALSE, edge.width=0.1)
 	dev.off()
 }
