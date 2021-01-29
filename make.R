87408f81a975a302891105880f690d4b547075a7
Alex <atruchon@vols.utk.edu>
2021-01-29 10:15:02 -0500

    Added a new plan for collecting Aureococcus data

--- make.R
+++ make.R
@@ -1,8 +1,13 @@
 source("R/packages.R")  # loads packages
 source("R/functions.R") # defines the create_plot() function
 source("R/plan.R")      # creates the drake plan
+source("R/data_plan.R") # creates a drake plan for pulling and checking Aureococcus data
 
 
 make(
   plan
+)
+
+make(
+  data_plan
 )
