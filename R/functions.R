ed03b0d91654d9799ad5b00ea873cb63ca5f930a
Alex <atruchon@vols.utk.edu>
2021-01-29 10:11:54 -0500

    Added a radial tree output as a test.

--- R/functions.R
+++ R/functions.R
@@ -15,6 +15,7 @@ plot_tree <- function(phy, file) {
 	pdf(file=file, width = 20, height = 20)
 	plot(phy)
 	plot(phy, type="fan")
-	plot(phy, type = "fan", show.tip.label=FALSE, edge.width=0.1)
+	#plot(phy, type = "fan", show.tip.label=FALSE, edge.width=0.1)
+	plot(phy, type="radial", edge.width = 0.25)
 	dev.off()
 }
