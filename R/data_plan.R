c4368fbb3ae59133427a601400fe2adbd07836d4
Alex <atruchon@vols.utk.edu>
2021-01-30 21:36:27 -0500

    Added source information for each data point

--- R/data_plan.R
+++ R/data_plan.R
@@ -3,7 +3,10 @@ data_plan <- drake_plan(
   Aureo.df = as.data.frame(Aureo.search$data),
   familyKey = Aureo.df[1,4],
   Pelago.count = occ_count(taxonKey=3750, georeferenced = TRUE),
-  Pelago.search = occ_search(taxonKey = "3750", fields=c('key', 'scientificName', 'basisOfRecord','protocol', 'taxonKey', 'year', 'phylum', 'order', 'family', 'class', 'decimalLatitude', 'decimalLongitude')),
+  Pelago.search = occ_search(taxonKey = "3750", fields=c('key', 'scientificName', 'basisOfRecord','protocol', 'taxonKey', 'year', 'phylum', 'order', 'family', 'class', 'decimalLatitude', 'decimalLongitude', 'materialSampleID'), limit = 20000),
   Pelago.df = as.data.frame(Pelago.search$data),
-  locations_sf <- st_as_sf(Aureo.df, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326),
+  Pelago.omit = Pelago.df[complete.cases(Pelago.df), ],
+  locations_sf = st_as_sf(Pelago.omit, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326),
   mapview(locations_sf))
+
+# Mapping the locations of all sampling sites for Pelagophytes should show all samples being derived from oceans, as pelagophytes are marine algae.
